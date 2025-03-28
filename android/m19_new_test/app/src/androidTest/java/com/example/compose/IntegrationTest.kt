c44daa7c322b09300746e111a3255a64dfd19711
Mikhail007008 <77855144+Mikhail007008@users.noreply.github.com>
2025-03-28 16:10:36 +0300

    Update IntegrationTest.kt

--- android/m19_new_test/app/src/androidTest/java/com/example/compose/IntegrationTest.kt
+++ android/m19_new_test/app/src/androidTest/java/com/example/compose/IntegrationTest.kt
@@ -3,6 +3,9 @@ package com.example.compose
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onFirst
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
@@ -15,10 +18,17 @@ class IntegrationTest {
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
-    fun testAppIntegration_showsCharacterName() {
+    fun testAppIntegration_showsLocationName() {
         composeTestRule.waitForIdle()
 
-        composeTestRule.onNodeWithText("Rick Sanchez")
+        composeTestRule.onNodeWithText("Locations").performClick()
+
+        composeTestRule.waitUntil(timeoutMillis = 5000) {
+            composeTestRule.onAllNodesWithTag("locationItem").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onAllNodesWithTag("locationItem")
+            .onFirst()
             .assertIsDisplayed()
     }
 }
