8a21c38b3e644c0f992089c6c75964a1dc7be3fe
Mikhail007008 <77855144+Mikhail007008@users.noreply.github.com>
2025-03-28 16:11:10 +0300

    Update UiTest.kt

--- android/m19_new_test/app/src/androidTest/java/com/example/compose/UiTest.kt
+++ android/m19_new_test/app/src/androidTest/java/com/example/compose/UiTest.kt
@@ -3,38 +3,22 @@ package com.example.compose
 import android.content.Context
 import android.content.Intent
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performScrollToIndex
-import androidx.paging.PagingData
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
-import com.example.compose.data.model.Character
-import com.example.compose.data.model.CharacterLocation
-import com.example.compose.data.model.Location
-import com.example.compose.ui.screen.CharactersScreen
-import com.example.compose.ui.viewModel.CharacterViewModel
-import com.example.compose.ui.viewModel.LocationViewModel
-import io.mockk.every
-import io.mockk.mockk
-import kotlinx.coroutines.flow.Flow
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.Rule
-import kotlinx.coroutines.flow.flowOf
-import org.mockito.Mock
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.`when`
 
 @RunWith(AndroidJUnit4::class)
 @MediumTest
 class MainScreenTest {
 
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
     fun testCharacterListDisplays() {
@@ -52,9 +36,8 @@ class MainScreenTest {
         val intent = Intent(context, MainActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
         context.startActivity(intent)
-        composeTestRule.waitForIdle()
 
         composeTestRule.onNodeWithText("Rick Sanchez").performClick()
-        composeTestRule.onNodeWithText("Character Details").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Person details").assertIsDisplayed()
     }
 }
